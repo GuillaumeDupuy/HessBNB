3cf8a7859e8701ce3fbee97b0a5b71c3817fa448
guillaume9320 <56391911+guillaume9320@users.noreply.github.com>
2020-06-10 10:11:57 +0200

    Template site Web

---
+++ site_web/web/inc/footer.inc.php
@@ -0,0 +1,112 @@
+<!-- footer -->
+<footer class="py-5">
+    <div class="container py-xl-4 py-lg-3">
+        <div class="row footer-grids">
+            <div class="col-lg-2 col-6 footer-grid">
+                <h3 class="mb-sm-4 mb-3 pb-lg-3">Acceuil</h3>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="index.html">Acceuil</a>
+                    </li>
+                    <li>
+                        <a href="#about">About Us</a>
+                    </li>
+                    <li>
+                        <a href="#services">Services</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-6 footer-grid">
+                <h3 class="mb-sm-4 mb-3 pb-lg-3">Compagnie</h3>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#find">Annonces</a>
+                    </li>
+                    <li>
+                        <a href="#places">Villes populaires</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-6 footer-grid footer-contact mt-lg-0 mt-4">
+                <h3 class="mb-sm-4 mb-3 pb-lg-3">Contact Us</h3>
+                <ul class="list-unstyled">
+                    <li>
+                        +33 6 48 64 31 78
+                    </li>
+                    <li>
+                        <a href="mailto:onsefoutdevotre@gueule.com">onsefoutdevotre@gueule.com</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-6 footer-grid text-lg-right">
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#find">Annonces</a>
+                    </li>
+                    <li>
+                        <a href="#apps">Apps</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-4 footer-grid mt-lg-0 mt-4">
+                <div class="footer-logo">
+                    <h2 class="text-lg-center text-center">
+                        <a class="logo text-wh font-weight-bold" href="index.html"><span class="text-bl">Air</span>Hess</a>
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- //footer -->
+<!-- copyright -->
+<div class="copyright-w3ls py-4">
+    <div class="container">
+        <div class="row">
+            <!-- copyright -->
+            <p class="col-lg-8 copy-right-grids text-wh text-lg-left text-center mt-lg-2">
+                © 2019 AirHess. All Rights Reserved | Design by
+                <a href="https://www.ynov-paris.com/?gclid=Cj0KCQjww_f2BRC-ARIsAP3zarHt1YCO6lzFJw5goGy1ei7Kc0I62hrCYlcCnPFHnjjUgGP2XmTKEb4aAmzbEALw_wcB" target="_blank">Ynov Paris Campus</a>
+            </p>
+            <!-- //copyright -->
+            <!-- social icons -->
+            <div class="col-lg-4 w3social-icons text-lg-right text-center mt-lg-0 mt-3">
+                <ul>
+                    <li>
+                        <a href="#" class="rounded-circle">
+                            <span class="fa fa-facebook-f"></span>
+                        </a>
+                    </li>
+                    <li class="px-2">
+                        <a href="#" class="rounded-circle">
+                            <span class="fa fa-google-plus"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="rounded-circle">
+                            <span class="fa fa-twitter"></span>
+                        </a>
+                    </li>
+                    <li class="pl-2">
+                        <a href="#" class="rounded-circle">
+                            <span class="fa fa-dribbble"></span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- //social icons -->
+        </div>
+    </div>
+</div>
+<!-- //copyright -->
+<!-- move top icon -->
+<a href="#home" class="move-top text-center">
+    <span class="fa fa-angle-double-up" aria-hidden="true"></span>
+</a>
+<!-- //move top icon -->
+</body>
+
+</html>
